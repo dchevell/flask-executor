083eb599ba2ba0ac9ffa6a3d24a9d262374716be
Dave Chevell <dchevell@atlassian.com>
2018-08-22 12:42:13 +1000

    Added support for decorating functions in the style of Celery or Dramatiq

--- flask_executor/__init__.py
+++ flask_executor/__init__.py
@@ -1,6 +1,5 @@
 import concurrent.futures
 
-from flask import current_app
 
 __all__ = ('Executor', )
 __version__ = '0.1.5'
@@ -17,21 +16,36 @@ class Executor:
     def init_app(self, app):
         app.config.setdefault('EXECUTOR_TYPE', 'thread')
         app.config.setdefault('EXECUTOR_MAX_WORKERS', None)
+        self._executor = self._make_executor(app)
         app.extensions['executor'] = self
 
-    def _make_executor(self):
-        executor_type = current_app.config['EXECUTOR_TYPE']
-        executor_max_workers = current_app.config['EXECUTOR_MAX_WORKERS']
+    def _make_executor(self, app):
+        executor_type = app.config['EXECUTOR_TYPE']
+        executor_max_workers = app.config['EXECUTOR_MAX_WORKERS']
         if executor_type == 'thread':
             _executor = concurrent.futures.ThreadPoolExecutor
         elif executor_type == 'process':
             _executor = concurrent.futures.ProcessPoolExecutor
         else:
-            raise ValueError('{} is not a valid executor type.'.format(executor_type))
+            raise ValueError("{} is not a valid executor type.".format(executor_type))
         return _executor(max_workers=executor_max_workers)
 
     def submit(self, *args, **kwargs):
-        if self._executor is None:
-            self._executor = self._make_executor()
         return self._executor.submit(*args, **kwargs)
 
+    def job(self, fn):
+        if type(self._executor) == concurrent.futures.ProcessPoolExecutor:
+            raise TypeError("Can't decorate {}: Executors that use multiprocessing"
+                            " don't support decorators".format(fn))
+        return ExecutorJob(executor=self, fn=fn)
+
+
+class ExecutorJob:
+
+    def __init__(self, executor, fn):
+        self.executor = executor
+        self.fn = fn
+
+    def submit(self, *args, **kwargs):
+        future = self.executor.submit(self.fn, *args, **kwargs)
+        return future
