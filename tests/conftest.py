0c4713cefab86b682b16421aafd0b2724405446c
Dave Chevell <dchevell@atlassian.com>
2018-09-21 08:16:11 +1000

    Cleaned up tests to run parameterised by default, so process and thread executors use the same tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,17 @@
 from flask import Flask
 import pytest
 
+from flask_executor import Executor
+
+
+@pytest.fixture(params=['thread', 'process'])
+def app(request):
+    app = Flask(__name__)
+    app.config['EXECUTOR_TYPE'] = request.param
+    return app
+
 @pytest.fixture
-def app():
+def default_app():
     app = Flask(__name__)
     return app
+
--- tests/test_executor.py
+++ tests/test_executor.py
@@ -7,9 +7,6 @@ import pytest
 from flask_executor import Executor, ExecutorJob
 
 
-EXECUTOR_MAX_WORKERS = 10
-
-
 # Reusable functions for tests
 
 def fib(n):
@@ -28,76 +25,68 @@ def g_context_test_value(_=None):
     return g.test_value
 
 
-# Begin tests
-
 def test_init(app):
     executor = Executor(app)
     assert 'executor' in app.extensions
+    assert isinstance(executor._executor, concurrent.futures._base.Executor)
 
 def test_factory_init(app):
     executor = Executor()
     executor.init_app(app)
     assert 'executor' in app.extensions
+    assert isinstance(executor._executor, concurrent.futures._base.Executor)
 
-def test_default_executor(app):
+def test_thread_executor_init(default_app):
+    default_app.config['EXECUTOR_TYPE'] = 'thread'
+    executor = Executor(default_app)
+    assert isinstance(executor._executor, concurrent.futures.ThreadPoolExecutor)
+
+def test_process_executor_init(default_app):
+    default_app.config['EXECUTOR_TYPE'] = 'process'
+    executor = Executor(default_app)
+    assert isinstance(executor._executor, concurrent.futures.ProcessPoolExecutor)
+
+def test_default_executor_init(default_app):
+    executor = Executor(default_app)
+    assert isinstance(executor._executor, concurrent.futures.ThreadPoolExecutor)
+
+def test_submit(app):
     executor = Executor(app)
-    with app.app_context():
-        executor.submit(fib, 5)
-        assert isinstance(executor._executor, concurrent.futures.ThreadPoolExecutor)
+    future = executor.submit(fib, 5)
+    assert future.result() == fib(5)
 
-def test_thread_executor(app):
-    app.config['EXECUTOR_TYPE'] = 'thread'
-    executor = Executor(app)
-    with app.app_context():
-        executor.submit(fib, 5)
-        assert isinstance(executor._executor, concurrent.futures.ThreadPoolExecutor)
-
-def test_process_executor(app):
-    app.config['EXECUTOR_TYPE'] = 'process'
-    executor = Executor(app)
-    with app.app_context():
-        executor.submit(fib, 5)
-        assert isinstance(executor._executor, concurrent.futures.ProcessPoolExecutor)
-
-def test_submit_result(app):
-    executor = Executor(app)
-    with app.app_context():
-        future = executor.submit(fib, 5)
-        assert isinstance(future, concurrent.futures.Future)
-        assert future.result() == fib(5)
-
-def test_thread_workers(app):
-    app.config['EXECUTOR_TYPE'] = 'thread'
+def test_max_workers(app):
+    EXECUTOR_MAX_WORKERS = 10
     app.config['EXECUTOR_MAX_WORKERS'] = EXECUTOR_MAX_WORKERS
     executor = Executor(app)
-    with app.app_context():
-        executor.submit(fib, 5)
-        assert executor._executor._max_workers == EXECUTOR_MAX_WORKERS
+    assert executor._executor._max_workers == EXECUTOR_MAX_WORKERS
 
-def test_process_workers(app):
-    app.config['EXECUTOR_TYPE'] = 'process'
-    app.config['EXECUTOR_MAX_WORKERS'] = EXECUTOR_MAX_WORKERS
-    executor = Executor(app)
-    with app.app_context():
-        executor.submit(fib, 5)
-        assert executor._executor._max_workers == EXECUTOR_MAX_WORKERS
-
-def test_thread_decorator(app):
-    app.config['EXECUTOR_TYPE'] = 'thread'
-    executor = Executor(app)
+def test_thread_decorator_submit(default_app):
+    default_app.config['EXECUTOR_TYPE'] = 'thread'
+    executor = Executor(default_app)
     @executor.job
     def decorated(n):
         return fib(n)
-    assert isinstance(decorated, ExecutorJob)
-    with app.app_context():
+    with default_app.test_request_context(''):
         future = decorated.submit(5)
-        assert isinstance(future, concurrent.futures.Future)
-        assert future.result() == fib(5)
+    assert future.result() == fib(5)
 
-def test_process_decorator(app):
+def test_thread_decorator_map(default_app):
+    iterable = list(range(5))
+    default_app.config['EXECUTOR_TYPE'] = 'thread'
+    executor = Executor(default_app)
+    @executor.job
+    def decorated(n):
+        return fib(n)
+    with default_app.test_request_context(''):
+        results = decorated.map(iterable)
+    for i, r in zip(iterable, results):
+        assert fib(i) == r
+
+def test_process_decorator(default_app):
     ''' Using decorators should fail with a TypeError when using the ProcessPoolExecutor '''
-    app.config['EXECUTOR_TYPE'] = 'process'
-    executor = Executor(app)
+    default_app.config['EXECUTOR_TYPE'] = 'process'
+    executor = Executor(default_app)
     try:
         @executor.job
         def decorated(n):
@@ -107,80 +96,46 @@ def test_process_decorator(app):
     else:
         assert 0
 
-def test_app_context_thread(app):
+def test_submit_app_context(app):
     test_value = random.randint(1, 101)
-    app.config['EXECUTOR_TYPE'] = 'thread'
     app.config['TEST_VALUE'] = test_value
     executor = Executor(app)
     future = executor.submit(app_context_test_value)
     assert future.result() == test_value
 
-def test_request_context_thread(app):
+def test_submit_g_context_process(app):
     test_value = random.randint(1, 101)
-    app.config['EXECUTOR_TYPE'] = 'thread'
-    executor = Executor(app)
-    with app.test_request_context('/'):
-        request.test_value = test_value
-        future = executor.submit(request_context_test_value)
-    assert future.result() == test_value
-
-def test_g_context_thread(app):
-    test_value = random.randint(1, 101)
-    app.config['EXECUTOR_TYPE'] = 'thread'
     executor = Executor(app)
     with app.test_request_context(''):
         g.test_value = test_value
         future = executor.submit(g_context_test_value)
     assert future.result() == test_value
 
-def test_app_context_process(app):
+def test_submit_request_context(app):
     test_value = random.randint(1, 101)
-    app.config['EXECUTOR_TYPE'] = 'process'
-    app.config['TEST_VALUE'] = test_value
-    executor = Executor(app)
-    future = executor.submit(app_context_test_value)
-    assert future.result() == test_value
-
-def test_request_context_process(app):
-    test_value = random.randint(1, 101)
-    app.config['EXECUTOR_TYPE'] = 'process'
     executor = Executor(app)
     with app.test_request_context(''):
         request.test_value = test_value
         future = executor.submit(request_context_test_value)
     assert future.result() == test_value
-
-def test_g_context_process(app):
-    test_value = random.randint(1, 101)
-    app.config['EXECUTOR_TYPE'] = 'process'
-    executor = Executor(app)
-    with app.test_request_context(''):
-        g.test_value = test_value
-        future = executor.submit(g_context_test_value)
-    assert future.result() == test_value
-
-def test_map_thread(app):
-    iterable = list(range(1, 6))
-    app.config['EXECUTOR_TYPE'] = 'thread'
-    executor = Executor(app)
-    results = executor.map(fib, iterable)
-    for i, r in zip(iterable, results):
-        assert fib(i) == r
-
-def test_map_process(app):
-    iterable = list(range(1, 6))
-    app.config['EXECUTOR_TYPE'] = 'process'
-    executor = Executor(app)
-    results = executor.map(fib, iterable)
-    for i, r in zip(iterable, results):
-        assert fib(i) == r
 
 def test_map_app_context(app):
     test_value = random.randint(1, 101)
     iterator = list(range(5))
     app.config['TEST_VALUE'] = test_value
     executor = Executor(app)
-    results = executor.map(app_context_test_value, iterator)
+    with app.test_request_context(''):
+        results = executor.map(app_context_test_value, iterator)
+    for r in results:
+        assert r == test_value
+
+def test_map_g_context_process(app):
+    test_value = random.randint(1, 101)
+    iterator = list(range(5))
+    executor = Executor(app)
+    with app.test_request_context(''):
+        g.test_value = test_value
+        results = executor.map(g_context_test_value, iterator)
     for r in results:
         assert r == test_value
 
@@ -193,23 +148,3 @@ def test_map_request_context(app):
         results = executor.map(request_context_test_value, iterator)
     for r in results:
         assert r == test_value
-
-def test_map_g_context(app):
-    test_value = random.randint(1, 101)
-    iterator = list(range(5))
-    executor = Executor(app)
-    with app.test_request_context(''):
-        g.test_value = test_value
-        results = executor.map(g_context_test_value, iterator)
-    for r in results:
-        assert r == test_value
-
-def test_map_decorator(app):
-    iterable = list(range(5))
-    executor = Executor(app)
-    @executor.job
-    def decorated(n):
-        return fib(n)
-    results = decorated.map(iterable)
-    for i, r in zip(iterable, results):
-        assert fib(i) == r
