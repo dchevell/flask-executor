268d86d740c9f6a95f0a14e101235592a6892cd8
Dave Chevell <dchevell@atlassian.com>
2018-09-24 11:01:20 +1000

    Added initial docstrings

--- flask_executor/__init__.py
+++ flask_executor/__init__.py
@@ -58,6 +58,8 @@ class Executor:
             self.init_app(app)
 
     def init_app(self, app):
+        """Initialise application.
+        """
         app.config.setdefault('EXECUTOR_TYPE', 'thread')
         executor_type = app.config['EXECUTOR_TYPE']
         executor_max_workers = default_workers(executor_type)
@@ -83,14 +85,77 @@ class Executor:
         return fn
 
     def submit(self, fn, *args, **kwargs):
+        """Schedules the callable, fn, to be executed as fn(\*args \**kwargs) and returns a
+        :class:`~concurrent.futures.Future` object representing the execution of the callable.
+
+        See also :meth:`concurrent.futures.Executor.submit`.
+
+        Callables are wrapped a copy of the current application context and the current request
+        context. Code that depends on information or configuration stored in
+        :data:`flask.current_app`, :data:`flask.request` or :data:`flask.g` can be run without
+        modification.
+
+        Note: Because callables only have access to *copies* of the application or request contexts
+        any changes made to these copies will not be reflected in the original view. Further,
+        changes in the original app or request context that occur after the callable is submitted
+        will not be available to the callable.
+
+        Example::
+
+            future = executor.submit(pow, 323, 1235)
+            print(future.result())
+
+        :param fn: The callable to be executed.
+        :param \*args: A list of positional parameters used with
+                       the callable.
+        :param \**kwargs: A dict of named parameters used with
+                          the callable.
+
+        :rtype: concurrent.futures.Future
+        """
         fn = self._prepare_fn(fn)
         return self._executor.submit(fn, *args, **kwargs)
 
     def map(self, fn, *iterables, **kwargs):
+        """Submits the callable, fn, and an iterable of arguments to the executor and returns the
+        results inside a generator.
+
+        See also :meth:`concurrent.futures.Executor.map`.
+
+        Callables are wrapped a copy of the current application context and the current request
+        context. Code that depends on information or configuration stored in
+        :data:`flask.current_app`, :data:`flask.request` or :data:`flask.g` can be run without
+        modification.
+
+        Note: Because callables only have access to *copies* of the application or request contexts
+        any changes made to these copies will not be reflected in the original view. Further,
+        changes in the original app or request context that occur after the callable is submitted
+        will not be available to the callable.
+
+        :param fn: The callable to be executed.
+        :param \*iterables: An iterable of arguments the callable will apply to.
+        :param \**kwargs: A dict of named parameters to pass to the underlying executor's
+                          :meth:`~concurrent.futures.Executor.map` method.
+        """
         fn = self._prepare_fn(fn)
         return self._executor.map(fn, *iterables, **kwargs)
 
     def job(self, fn):
+        """Decorator. Use this to transform functions into `ExecutorJob` instances that can submit
+        themselves directly to the executor.
+
+        Example::
+
+            @executor.job
+            def fib(n):
+                if n <= 2:
+                    return 1
+                else:
+                    return fib(n-1) + fib(n-2)
+
+            future = fib.submit(5)
+            results = fib.map(range(1, 6))
+        """
         if isinstance(self._executor, concurrent.futures.ProcessPoolExecutor):
             raise TypeError("Can't decorate {}: Executors that use multiprocessing"
                             " don't support decorators".format(fn))
