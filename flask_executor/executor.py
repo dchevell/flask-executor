9ec3a3dd2288d872406a33e5e6af9eac9890b694
Dave Chevell <chevell@gmail.com>
2022-08-15 08:08:14 +1000

    Make pushing app context default behaviour

--- flask_executor/executor.py
+++ flask_executor/executor.py
@@ -2,7 +2,7 @@ import concurrent.futures
 import re
 
 from flask import copy_current_request_context
-from flask.globals import _app_ctx_stack, current_app
+from flask.globals import current_app
 
 from flask_executor.futures import FutureCollection, FutureProxy
 from flask_executor.helpers import InstanceProxy, str2bool
@@ -13,16 +13,6 @@ def push_app_context(fn):
 
     def wrapper(*args, **kwargs):
         with app.app_context():
-            return fn(*args, **kwargs)
-
-    return wrapper
-
-
-def copy_current_app_context(fn):
-    app_context = _app_ctx_stack.top
-
-    def wrapper(*args, **kwargs):
-        with app_context:
             return fn(*args, **kwargs)
 
     return wrapper
@@ -93,7 +83,7 @@ class Executor(InstanceProxy, concurrent.futures._base.Executor):
             * :class:`concurrent.futures.ProcessPoolExecutor`
         """
         app.config.setdefault(self.EXECUTOR_TYPE, 'thread')
-        app.config.setdefault(self.EXECUTOR_PUSH_APP_CONTEXT, False)
+        app.config.setdefault(self.EXECUTOR_PUSH_APP_CONTEXT, True)
         futures_max_length = app.config.setdefault(self.EXECUTOR_FUTURES_MAX_LENGTH, None)
         propagate_exceptions = app.config.setdefault(self.EXECUTOR_PROPAGATE_EXCEPTIONS, False)
         if futures_max_length is not None:
@@ -122,8 +112,6 @@ class Executor(InstanceProxy, concurrent.futures._base.Executor):
             fn = copy_current_request_context(fn)
             if current_app.config[self.EXECUTOR_PUSH_APP_CONTEXT]:
                 fn = push_app_context(fn)
-            else:
-                fn = copy_current_app_context(fn)
         return fn
 
     def submit(self, fn, *args, **kwargs):
